6957095c41581eb8f37e518587a7a7e60547d4dd
Andres Glasener <52112240+Aglasener@users.noreply.github.com>
2020-02-03 15:55:04 -0500

    added the html routes and api routes to server.js

---
+++ db/db.js
@@ -0,0 +1,11 @@
+var savedNotes = [
+    {
+        id: "0",
+        title: "test",
+        body: "testbody"
+    }
+];
+    
+
+module.exports = savedNotes;
+
---
+++ public/server.js
@@ -0,0 +1,63 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+var savedNotes = require("savedNotes");
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Basic HTML routes
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+// Reads the db.json file and return all saved notes as JSON
+app.get("/api/notes", function(req, res) {
+  return res.json(savedNotes);
+});
+
+
+// Recieve a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
+
+app.post("/api/notes", function(req, res) {
+  var newNote = req.body;
+  newNote.id = savedNotes.length;
+
+  console.log(newNote);
+
+  savedNotes.push(newNote);
+
+  res.json(newNote);
+});
+
+app.delete("api/notes/:id", function(req, res) {
+  var deleteNote = req.params.savedNotes;
+
+  console.log(deleteNote);
+
+  for (var i = 0; i <savedNotes.length; i++){
+    if(deleteNote === savedNotes[i].id){
+      res.delete(savedNotes[i]);
+      return res.json(savedNotes);
+    }
+  }
+
+  return res.json(false);
+});
+
+// Start server
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
