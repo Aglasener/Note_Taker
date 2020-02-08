ebc0e6de3fdabcc5331f6d29dbe2cbfba8c882be
Andres Glasener <52112240+Aglasener@users.noreply.github.com>
2020-02-08 12:31:35 -0500

    finsihed the delete part of the app

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const readFileAsync = util.promisify(fs.readFile);
 var app = express();
 var PORT = 3000;
 
-console.log(readFileAsync("../db/database.json"));
+
 // var test = '{"id": 0,"title": "test", "text": "testbody"}';
 
 // var result = JSON.parse(test);
@@ -22,20 +22,25 @@ console.log(readFileAsync("../db/database.json"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/assets", express.static('./assets/'));
+app.use(express.static("public"));
+
+//change html routes
 
 //Basic HTML routes
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "public","index.html"));
   });
 
 app.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "notes.html"));
+  res.sendFile(path.join(__dirname, "public","notes.html"));
 });
 
 // Reads the db.json file and return all saved notes as JSON
 app.get("/api/notes", function(req, res) {
-  return res.json(JSON.parse("../db/database.json"));
+  let contents = fs.readFileSync(path.join(__dirname, "db", "database.json"));
+  let parsedContents =  JSON.parse(contents);
+  return res.json(parsedContents);
 });
 
 
@@ -43,38 +48,42 @@ app.get("/api/notes", function(req, res) {
 
 app.post("/api/notes", function(req, res) {
   var newNote = req.body;
-  let savedNotes = JSON.parse("../db/database.json");
+  let savedNotes = JSON.parse(fs.readFileSync(path.join(__dirname, "db", "database.json")));
   newNote.id = savedNotes.length;
 
   console.log(newNote);
 
   savedNotes.push(newNote);
   
-  writeFileAsync("../db/database.json", JSON.stringify(savedNotes));
+  writeFileAsync("./db/database.json", JSON.stringify(savedNotes));
 
-  res.json(newNote);
+  return res.json(newNote);
 });
 
-// app.delete("/api/notes/:id", function(req, res) {
-//   var deleteNote = req.params.id;
-//   let savedNotes = JSON.parse(readFileAsync("../db/database.json"));
-//   console.log(deleteNote);
+app.delete("/api/notes/:id", function(req, res) {
+  var deleteNote = Number(req.params.id);
+  
+  let contents = fs.readFileSync(path.join(__dirname, "db", "database.json"));
+  let savedNotes =  JSON.parse(contents);
+  console.log(savedNotes.length);
+  for (var i = 0; i <savedNotes.length; i++){
+    if(deleteNote === savedNotes[i].id){
+      savedNotes.splice(i, 1);
+      
+      writeFileAsync("./db/database.json", JSON.stringify(savedNotes));
+      //fs write or replace the string only in database.js with new one in order to create a persistance database info
+      let parsedContents =  JSON.parse(contents);
+      return res.json(parsedContents);
 
-//   for (var i = 0; i <savedNotes.length; i++){
-//     if(deleteNote === savedNotes[i].id){
-//       savedNotes.splice(i, 1);
+    }
+  }
 
-//       writeFileAsync("../db/database.json", JSON.stringify(savedNotes));
-//       //fs write or replace the string only in database.js with new one in order to create a persistance database info
-//       return res.json(savedNotes);
-
-//     }
-//   }
-
-//   return res.json(false);
-// });
+  return res.json(false);
+});
 
 // Start server
+
+
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
